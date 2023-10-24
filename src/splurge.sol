402fc79e28d1c52d89a39279ba6868466af6c98d
Rohith Venkatesan <rohithvenkatesan@Rohiths-MacBook-Pro.local>
2023-10-24 18:26:53 -0400

    traded tokens are not withdrawable

--- src/splurge.sol
+++ src/splurge.sol
@@ -18,30 +18,32 @@ contract splurge {
 
     function executeTrade(address inputTokenAddy, address outputTokenAddy, address user, uint amount, bytes memory swapCallData /*, bytes memory signature*/) public {
         IERC20 token = IERC20(inputTokenAddy);
+        // user approves our contract
         token.transferFrom(user, address(this), amount);
-        require(token.balanceOf(address(this)) == amount, "didnt send properly");
+        uint initialBalance = token.balanceOf(address(this));
         token.approve(swapRouter, amount);
 
         (bool success,) = swapRouter.call(swapCallData);
-        require(success, "swap call failed");
+        uint afterBalance = token.balanceOf(address(this));
+        uint amountChange = afterBalance - initialBalance;
 
+        require(success && amountChange > 0, "swap call failed");
        //(bool success, bytes memory returnData) = swapRouter.call(swapCallData);
+       // uint outputAmount = abi.decode(returnData, (uint256));
 
-        //uint outputAmount = abi.decode(returnData, (uint256));
-        //tokenBalances[user][outputTokenAddy] += outputAmount;
+        tokenBalances[user][outputTokenAddy] += amountChange;
     }
-    /*
-
+    
     function withdrawBalances(address[] calldata tokensToWithdraw, uint[] calldata amounts) public {
         for(uint i = 0; i < tokensToWithdraw.length; i++){
             IERC20 token = IERC20(tokensToWithdraw[i]);
             require(tokenBalances[msg.sender][tokensToWithdraw[i]] >= amounts[i]);
-            token.transferFrom(address(this), msg.sender, amounts[i]);
+            token.transfer(msg.sender, amounts[i]);
             tokenBalances[msg.sender][tokensToWithdraw[i]] -= amounts[i];
         }
         
     }
-    */
+    
 
 
     function verifyTrade(bytes memory message, uint8 v, bytes32 r, bytes32 s) public pure returns (address){ 
