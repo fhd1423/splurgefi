af31875d45eacdfc48ff99fd71db1e503ccc111b
Rohith Venkatesan <rohithvenkatesan@Rohiths-MacBook-Pro.local>
2023-10-29 14:16:08 -0400

    trade in tranches, linting

--- src/splurge.sol
+++ src/splurge.sol
@@ -1,40 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-struct orderStruct {
+struct OrderStruct {
     address inputTokenAddy;
     address outputTokenAddy;
     address recipient; // the user we are executing the trade for
-    uint amount;
-    uint tranches;
-    uint deadline; // when the order expires
-    uint salt; // random number
+    uint256 amount;
+    uint8 tranches;
+    uint256 deadline; // when the order expires
+    uint8 salt; // random number
 }
 
 interface IWETH is IERC20 {
-    function withdraw(uint wad) external;
+    function withdraw(uint256 wad) external;
 }
 
-contract splurge {
+contract Splurge is ReentrancyGuard {
     address public swapRouter;
-    IWETH public WETH;
-    address wethAddress;
-    mapping(address => mapping(address => uint)) tokenBalances;
-    mapping(bytes => uint) tranchesCompleted;
+    IWETH internal wETH;
+    address internal wethAddress;
+    mapping(address => mapping(address => uint256)) public tokenBalances;
+    mapping(bytes => uint256) public tranchesCompleted;
 
     constructor(address _swapRouter, address _wethAddress) {
         // 0x router address
         swapRouter = _swapRouter;
         // wmatic for testing purposes
         wethAddress = _wethAddress;
-        WETH = IWETH(_wethAddress);
+        wETH = IWETH(_wethAddress);
     }
 
     function prepareVerifyTrade(
-        orderStruct memory order,
+        OrderStruct memory order,
         bytes memory signature,
         bytes memory swapCallData
     ) public {
@@ -50,42 +51,50 @@ contract splurge {
 
         require(
             verifyTrade(concatenatedOrderBytesBeforeHash, signature) ==
-                order.recipient
+                order.recipient,
+            "sig didnt match"
         );
         executeTrade(order, signature, swapCallData);
     }
 
     function executeTrade(
-        orderStruct memory order,
+        OrderStruct memory order,
         bytes memory signature,
         bytes memory swapCallData
-    ) public returns (bool) {
+    ) public nonReentrant returns (bool) {
         IERC20 input = IERC20(order.inputTokenAddy);
         IERC20 output = IERC20(order.outputTokenAddy);
         require(
             order.inputTokenAddy == wethAddress ||
-                order.outputTokenAddy == wethAddress
+                order.outputTokenAddy == wethAddress,
+            "trade path must include wETH"
         );
 
-        require(tranchesCompleted[signature] < order.tranches);
+        require(
+            tranchesCompleted[signature] < order.tranches,
+            "tranches completed already"
+        );
 
-        uint tranche = order.amount / order.tranches;
+        uint256 tranche = order.amount / order.tranches;
         input.transferFrom(order.recipient, address(this), tranche);
         if (order.inputTokenAddy == wethAddress) unwrapAndPay();
 
         // @todo optimize this, maybe only need to approve once
         input.approve(swapRouter, tranche);
 
-        uint initialBalance = output.balanceOf(address(this));
+        uint256 initialBalance = output.balanceOf(address(this));
 
         (bool success, ) = swapRouter.call(swapCallData);
         // refund on fail
-        if (!success) input.transfer(order.recipient, tranche);
-        uint afterBalance = output.balanceOf(address(this));
-        uint amountChange = afterBalance - initialBalance;
+        if (!success) {
+            input.transfer(order.recipient, tranche);
+            return false;
+        }
+        uint256 afterBalance = output.balanceOf(address(this));
+        uint256 amountChange = afterBalance - initialBalance;
 
         if (order.outputTokenAddy == wethAddress) {
-            require(amountChange > 1e16);
+            require(amountChange > 1e16, "not enough for fee");
             amountChange -= 1e16;
             unwrapAndPay();
         }
@@ -99,12 +108,13 @@ contract splurge {
 
     function withdrawBalances(
         address[] calldata tokensToWithdraw,
-        uint[] calldata amounts
-    ) public {
-        for (uint i = 0; i < tokensToWithdraw.length; i++) {
+        uint256[] calldata amounts
+    ) public nonReentrant {
+        for (uint8 i = 0; i < tokensToWithdraw.length; i++) {
             IERC20 token = IERC20(tokensToWithdraw[i]);
             require(
-                tokenBalances[msg.sender][tokensToWithdraw[i]] >= amounts[i]
+                tokenBalances[msg.sender][tokensToWithdraw[i]] >= amounts[i],
+                "cant withdraw above balance"
             );
             token.transfer(msg.sender, amounts[i]);
             tokenBalances[msg.sender][tokensToWithdraw[i]] -= amounts[i];
@@ -120,8 +130,11 @@ contract splurge {
     }
 
     function unwrapAndPay() private {
-        require(WETH.balanceOf(address(this)) > 1e16); // 0.01 eth
-        WETH.withdraw(1e16);
+        require(
+            wETH.balanceOf(address(this)) > 1e16,
+            "must have enough for tx fee"
+        ); // 0.01 eth
+        wETH.withdraw(1e16);
         payable(msg.sender).transfer(1e16);
     }
 
