b0fcb98c0cb840729ab76686920db275b9531951
“Karthik144” <karthik.k.ramu@gmail.com>
2023-10-29 20:07:53 -0400

    Added basic landing page for testing

--- offchain/frontend/tailwind.config.js
+++ offchain/frontend/tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -17,4 +16,3 @@ const config: Config = {
   },
   plugins: [],
 }
-export default config
