4e3a119bd2cc7652c03ee5c149c7796326fb0639
AkhilMah14 <akhil@maheeps.net>
2023-11-04 16:00:48 -0400

    folder restructure

--- src/frontend/app/page.js
+++ src/frontend/app/page.js
@@ -1,13 +1,23 @@
 import LandingPage from "../components/LandingPage";
 import FeaturesSection from "../components/FeaturesSection";
 import WaitlistSection from "../components/WaitlistSection";
+import { DynamicContextProvider, DynamicWidget } from '@dynamic-labs/sdk-react-core';
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 
 export default function Home() {
   return (
-    <div className="flex flex-col bg-black font-sans w-full">
-      <LandingPage />
-      <FeaturesSection />
-      <WaitlistSection />
-    </div>
+    <DynamicContextProvider
+      settings={{
+        environmentId: '91cf8e2a-5722-4141-9da9-982b3cdbb800',
+        walletConnectors: [EthereumWalletConnectors],
+      }}>
+      <DynamicWidget />
+
+      <div className="flex flex-col bg-black font-sans w-full">
+        <LandingPage />
+        <FeaturesSection />
+        <WaitlistSection />
+      </div>
+    </DynamicContextProvider>
   );
 }
