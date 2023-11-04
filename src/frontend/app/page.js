ab3bbd1d64542312e7ca6511e0fe0159c9886ace
“Karthik144” <karthik.k.ramu@gmail.com>
2023-11-04 16:48:27 -0400

    Added dynamic barebones

--- src/frontend/app/page.js
+++ src/frontend/app/page.js
@@ -2,15 +2,26 @@
 import LandingPage from "../components/LandingPage";
 import FeaturesSection from "../components/FeaturesSection";
 import WaitlistSection from "../components/WaitlistSection";
-// import { DynamicContextProvider, DynamicWidget } from '@dynamic-labs/sdk-react-core';
-// import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import {
+  DynamicContextProvider,
+  DynamicWidget,
+} from "@dynamic-labs/sdk-react-core";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 
 export default function Home() {
   return (
     <div className="flex flex-col bg-black font-sans w-full">
-      <LandingPage />
-      <FeaturesSection />
-      <WaitlistSection />
+      <DynamicContextProvider
+        settings={{
+          // Find your environment id at https://app.dynamic.xyz/dashboard/developer
+          environmentId: "8f61ad0e-bccc-44b2-a96e-148f47498674",
+          walletConnectors: [EthereumWalletConnectors],
+        }}
+      >
+        <LandingPage />
+        <FeaturesSection />
+        <WaitlistSection />
+      </DynamicContextProvider>
     </div>
   );
 }
