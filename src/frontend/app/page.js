01b4c5c97f96c345e709fe70da3058b9d1705001
Rohith Venkatesan <rohithvenkatesan@Rohiths-MacBook-Pro.local>
2023-11-04 16:08:13 -0400

    dynamic wallet connection

--- src/frontend/app/page.js
+++ src/frontend/app/page.js
@@ -1,13 +1,24 @@
+"use client";
 import LandingPage from "../components/LandingPage";
 import FeaturesSection from "../components/FeaturesSection";
 import WaitlistSection from "../components/WaitlistSection";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
 
 export default function Home() {
   return (
     <div className="flex flex-col bg-black font-sans w-full">
-      <LandingPage />
-      <FeaturesSection />
-      <WaitlistSection />
+      <DynamicContextProvider
+        settings={{
+          // Find your environment id at https://app.dynamic.xyz/dashboard/developer
+          environmentId: "8f61ad0e-bccc-44b2-a96e-148f47498674",
+          walletConnectors: [EthereumWalletConnectors],
+        }}
+      >
+        <LandingPage />
+        <FeaturesSection />
+        <WaitlistSection />
+      </DynamicContextProvider>
     </div>
   );
 }
--- src/frontend/components/WaitlistSection.js
+++ src/frontend/components/WaitlistSection.js
@@ -1,5 +1,7 @@
 "use client";
 
+import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
+
 function WaitlistSection() {
   // Function to handle click event
   const handleButtonClick = () => {
@@ -9,15 +11,12 @@ function WaitlistSection() {
 
   return (
     <div className="text-center mt-48 mb-48">
-      <h1 className="text-6xl mb-10 text-white font-bold tracking-wide leading-tight">
-        Join the waitlist today.
+      <h1 className="text-xl mb-10 text-white font-bold tracking-wide leading-tight">
+        it may be a good idea to link your twitter account as well 😏
       </h1>
-      <button
-        onClick={handleButtonClick} // Add the onClick event handler here
-        className="bg-green-500 text-white text-xl font-bold py-2 px-8 rounded-full shadow-lg hover:bg-green-600 focus:outline-none focus:ring focus:ring-green-200 focus:ring-opacity-50"
-      >
-        Join Waitlist
-      </button>
+      <h1 className="flex justify-center">
+        <DynamicWidget />
+      </h1>
     </div>
   );
 }
