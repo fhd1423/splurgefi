2f0c2eea1a731ddaf111ddd0b0b5de7c51b629d5
“Karthik144” <karthik.k.ramu@gmail.com>
2023-11-04 17:01:20 -0400

    Customized dynamic button

--- src/frontend/components/WaitlistSection.js
+++ src/frontend/components/WaitlistSection.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { useDynamicContext } from "@dynamic-labs/sdk-react-core";
 import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
 
 function WaitlistSection() {
@@ -9,14 +10,27 @@ function WaitlistSection() {
     window.location.href = "https://760abapqqfl.typeform.com/to/umui6CKW";
   };
 
+  const { setShowAuthFlow } = useDynamicContext();
+
   return (
     <div className="text-center mt-48 mb-48">
-      <h1 className="text-xl mb-10 text-white font-bold tracking-wide leading-tight">
-        it may be a good idea to link your twitter account as well 😏
+      <h1 className="text-5xl mb-5 text-white font-bold tracking-wide leading-tight">
+        Join our waitlist.
       </h1>
-      <h1 className="flex justify-center">
+      {/* <h1 className="flex justify-center">
         <DynamicWidget />
-      </h1>
+      </h1> */}
+
+      <p className="text-2xl mb-8 text-gray-400 tracking-wide">
+        Connect you wallet & twitter account to join.
+      </p>
+
+      <button
+        className="bg-green-500 text-white text-xl font-bold py-2 px-8 rounded-full shadow-lg hover:bg-green-600"
+        onClick={() => setShowAuthFlow(true)}
+      >
+        Connect Wallet
+      </button>
     </div>
   );
 }
