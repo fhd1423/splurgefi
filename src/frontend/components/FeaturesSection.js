f65c72fe23db88d80de57e6bf547ddfe57a8b153
AkhilMah14 <akhil@maheeps.net>
2023-11-04 13:37:27 -0400

    Ready for testing

---
+++ src/frontend/components/FeaturesSection.js
@@ -0,0 +1,24 @@
+import Feature from "./Feature";
+
+function FeaturesSection() {
+  return (
+    <div className="flex justify-around mt-10 mb-20 items-center">
+      {" "}
+      {/* Added mb-20 here */}
+      <Feature
+        title="Automated Trades"
+        description="Don’t waste time staring at charts."
+      />
+      <Feature
+        title="Optimized for Speed"
+        description="Easy, secure, and fast trading experience."
+      />
+      <Feature
+        title="Maximize Returns"
+        description="Split your automated trade into batches."
+      />
+    </div>
+  );
+}
+
+export default FeaturesSection;
