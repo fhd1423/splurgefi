2ac462dd4b9b26c0b93b856811e8d6afbfd6b332
“Karthik144” <karthik.k.ramu@gmail.com>
2023-11-05 20:25:54 -0500

    Cleaned up code

--- src/frontend/components/CustomDatePicker.js
+++ src/frontend/components/CustomDatePicker.js
@@ -5,81 +5,26 @@ import { styled } from "@mui/system";
 import Typography from "@mui/material/Typography";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 
-// // Custom styles for the input container if needed
-// const CustomDatePickerContainer = styled("div")({
-//   // Add any specific styles for the date picker container here if necessary
-// });
-
-// // You can use a similar approach to CustomInput for styling the TextField component
-// const CustomStyledTextField = styled(TextField)({
-//   "& label": {
-//     color: "white", // Label color
-//   },
-//   "& label.Mui-focused": {
-//     color: "white", // Label color when the input is focused
-//   },
-//   "& .MuiInputBase-input": {
-//     color: "white", // Input text color
-//   },
-//   "& .MuiOutlinedInput-root": {
-//     "& fieldset": {
-//       borderColor: "white", // Border color
-//     },
-//     "&:hover fieldset": {
-//       borderColor: "white", // Border color on hover
-//     },
-//     "&.Mui-focused fieldset": {
-//       borderColor: "white", // Border color when focused
-//     },
-//   },
-//   "& .MuiSvgIcon-root": {
-//     // This targets the icons inside the TextField component
-//     color: "white", // Icons color like the calendar icon
-//   },
-// });
-
-// export default function CustomDatePicker({ value, onChange, label, ...other }) {
-//   return (
-//     <CustomDatePickerContainer>
-//       <Typography
-//         variant="subtitle1"
-//         color="white"
-//         fontWeight="600"
-//         gutterBottom
-//       >
-//         {label}
-//       </Typography>
-//       <DatePicker
-//         value={value}
-//         onChange={onChange}
-//         renderInput={(params) => <CustomStyledTextField {...params} />}
-//         {...other}
-//       />
-//     </CustomDatePickerContainer>
-//   );
-// }
-
 const CustomStyledTextField = styled(TextField)({
   "& label.Mui-focused": {
-    color: "white", // Label color when the input is focused
+    color: "white",
   },
   "& .MuiInputBase-input": {
-    color: "white", // Input text color
+    color: "white",
   },
   "& .MuiOutlinedInput-root": {
     "& fieldset": {
-      borderColor: "white", // Border color
+      borderColor: "white",
     },
     "&:hover fieldset": {
-      borderColor: "white", // Border color on hover
+      borderColor: "white",
     },
     "&.Mui-focused fieldset": {
-      borderColor: "white", // Border color when focused
+      borderColor: "white",
     },
   },
   "& .MuiSvgIcon-root": {
-    // This targets the icons inside the TextField component
-    color: "white", // Icons color like the calendar icon
+    color: "white",
   },
 });
 
@@ -96,20 +41,19 @@ export default function CustomDatePicker({ value, onChange, label, ...other }) {
             InputLabelProps={{
               ...params.InputLabelProps,
               style: { color: "white" },
-            }} // Ensure the label is white
+            }}
             inputProps={{ ...params.inputProps, style: { color: "white" } }} // Ensure the input text is white
-            // Add additional styles for white outline
             sx={{
               "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-                borderColor: "white", // Default outline color
+                borderColor: "white",
               },
               "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline":
                 {
-                  borderColor: "white", // Outline color on hover
+                  borderColor: "white",
                 },
               "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
                 {
-                  borderColor: "white", // Outline color when focused
+                  borderColor: "white",
                 },
             }}
           />
