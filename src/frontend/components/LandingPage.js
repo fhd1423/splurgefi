75b23ed2208c95349a6255cc98ac2bb883458141
AkhilMah14 <akhil@maheeps.net>
2023-11-10 21:54:17 -0500

    merging frontend

--- src/frontend/components/LandingPage.js
+++ src/frontend/components/LandingPage.js
@@ -5,38 +5,6 @@ import { useEffect, useState } from "react";
 import { useDynamicContext, DynamicWidget } from "@dynamic-labs/sdk-react-core";
 import GradientText from "./GradientText";
 
-<<<<<<< HEAD
-const LandingPage = () => (
-  <div className="flex flex-col h-screen justify-center items-center bg-black font-sans">
-    <Head>
-      <title>SplurgeFi</title>
-      <meta name="description" content="Welcome to SplurgeFi" />
-      <link rel="icon" href="/favicon.ico" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-        rel="stylesheet"
-      />
-    </Head>
-
-    <div className="text-center">
-      <h1 className="text-6xl mb-4 text-white font-bold tracking-wide leading-tight">
-        Automate your trades
-      </h1>
-      <h1 className="text-6xl mb-8 text-white font-bold tracking-wide leading-tight">
-        on DEXs seamlessly
-      </h1>
-      <p className="text-2xl mb-10 text-gray-400 tracking-wide">
-        Best trading experience ever.
-      </p>
-      <Link href="/step-one" passHref>
-        <button className="bg-green-500 text-white text-xl font-bold py-2 px-8 rounded-full shadow-lg hover:bg-green-600">
-          Get Started
-        </button>
-      </Link>
-    </div>
-  </div>
-);
-=======
 const LandingPage = () => {
   // Local state to track if the wallet is connected
   const [isWalletConnected, setIsWalletConnected] = useState(false);
@@ -95,6 +63,5 @@ const LandingPage = () => {
     </div>
   );
 };
->>>>>>> 639d56fd9279e2a72ce47a426bfc6a12d420c650
 
 export default LandingPage;
