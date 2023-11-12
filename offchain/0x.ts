fc7168b8aa9aa34efae620444f091a41be560449
Rohith Venkatesan <rohithvenkatesan@Rohiths-MacBook-Pro.local>
2023-11-12 15:37:17 -0500

    fix duplicate offchain folder

--- offchain/0x.ts
+++ offchain/0x.ts
@@ -1,4 +1,10 @@
 const axios = require("axios");
+const { ethers } = require("ethers");
+
+const Splurge_ABI = [
+  "function transformERC20(address,address,uint256,uint256,(uint32, bytes)[]) public",
+];
+
 import PriceQueue from "./PriceQueue";
 import chunk from "./utils";
 
@@ -6,30 +12,13 @@ const priceQueues: Map<string, PriceQueue> = new Map();
 
 const pairs = [
   {
-    input: "0xbcdCB26fFec1bE5991FA4b5aF5B2BbC878965Db1",
-    output: "0xFA75399b5ce8C0299B0434E0D1bcFDFd8fF8a755",
-  },
-  {
-    input: "0xFA75399b5ce8C0299B0434E0D1bcFDFd8fF8a755",
-    output: "0xbcdCB26fFec1bE5991FA4b5aF5B2BbC878965Db1",
-  },
-  {
-    input: "0x50dbE194eC396E22A0796919C048323D6086E79D",
-    output: "0xF83Efca7dd4a646C288aC30A8F761383DCFF5306",
-  },
-  {
-    input: "0xF83Efca7dd4a646C288aC30A8F761383DCFF5306",
-    output: "0x50dbE194eC396E22A0796919C048323D6086E79D",
-  },
-  {
-    input: "0x193842E186561260DC49Bd1f5981bfDED1BD672D",
-    output: "0x58c330A4f6e783779a6d1A904555E9E5375d0255",
-  },
-  {
-    input: "0x58c330A4f6e783779a6d1A904555E9E5375d0255",
-    output: "0x193842E186561260DC49Bd1f5981bfDED1BD672D",
+    input: "0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889",
+    output: "0xa0a6c157871A9F38253234BBfD2B8D79F9e9FCDC",
   },
 ];
+
+const splurgeAddy = "0xf1523fcd98490383d079f5822590629c154cfacf";
+const splurgeContract = new ethers.Contract(splurgeAddy, Splurge_ABI);
 
 for (const pair of pairs) {
   const key = `${pair.input}:${pair.output}`;
@@ -52,6 +41,13 @@ async function fetchPrice(pair: {
       Date()
     );
 
+    let ZeroExCalldata = splurgeContract.interface.decodeFunctionData(
+      "transformERC20",
+      response.data.data
+    );
+
+    console.log(ZeroExCalldata);
+
     const key = `${pair.input}:${pair.output}`;
     const priceQueue = priceQueues.get(key);
     if (priceQueue) {
@@ -64,7 +60,8 @@ async function fetchPrice(pair: {
       console.error(`No PriceQueue found for ${key}`);
     }
   } catch (error) {
-    console.error(`Fetch failed for ${pair.input}:${pair.output}`);
+    console.log(error);
+    //console.error(`Fetch failed for ${pair.input}:${pair.output}`);
   }
 }
 
@@ -75,7 +72,7 @@ async function staggeredQuery(pairs: { input: string; output: string }[]) {
       setInterval(async () => {
         const fetchPromises = chunk.map((pair) => fetchPrice(pair));
         await Promise.all(fetchPromises);
-      }, 10000); // Fetch every 10 seconds
+      }, 3000); // Fetch every 10 seconds
     }, index * 1000); // stagger by 1 second
   });
 }
