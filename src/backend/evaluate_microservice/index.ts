f4607c2c758472dde4fc6485188aa16ed0613041
Rohith Venkatesan <rohithvenkatesan@Rohiths-MacBook-Pro.local>
2023-11-21 16:57:12 -0500

    fix trade evalution

--- src/backend/evaluate_microservice/index.ts
+++ src/backend/evaluate_microservice/index.ts
@@ -1,7 +1,6 @@
 const axios = require('axios');
 const { createClient } = require('@supabase/supabase-js');
-import { ethers } from 'ethers';
-
+import { ethers, JsonRpcProvider, Contract } from 'ethers';
 
 //Set global API & Client Instances
 const supabase = createClient(
@@ -9,31 +8,30 @@ const supabase = createClient(
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdtdXBleHhxbnpycnpvemNvdmpwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5OTIxOTEyNywiZXhwIjoyMDE0Nzk1MTI3fQ.iF0xiz-vE5tx52u4soGJbEtGHtIB_EyQFFU_eB5dVak',
 );
 
-// Fetch all events based on signature 
+// Fetch all events based on signature
 const getContractLogEvents = async (signature: string) => {
-
-  // Contract & Ethers setup 
-  const provider = new ethers.providers.JsonRpcProvider('https://eth-mainnet.g.alchemy.com/v2/oQcwBviequQgqzvgAXUShhOlCzgZKtKK');
+  // Contract & Ethers setup
+  const provider = new JsonRpcProvider(
+    'https://eth-mainnet.g.alchemy.com/v2/oQcwBviequQgqzvgAXUShhOlCzgZKtKK',
+  );
   const contractAddress = 'CONTRACT_ADDRESS';
-  const contractABI = 'CONTRACT_ABI'; 
+  const contractABI = 'CONTRACT_ABI';
 
-  const contract = new ethers.Contract(contractAddress, contractABI, provider);
+  const contract = new Contract(contractAddress, contractABI, provider);
 
   try {
-
     const eventFilter = contract.filters.TradeEvent();
     const events = await contract.queryFilter(eventFilter);
-    const filteredEvents = events.filter(event => event.args && event.args._signature === signature);
+    // below line has an error
+    const filteredEvents = events.filter(
+      (event) => event.args && event.args._signature === signature,
+    );
     return filteredEvents;
-
   } catch (error) {
-
-    console.error("Error fetching contract events:", error);
+    console.error('Error fetching contract events:', error);
     throw error;
-
   }
-
-}; 
+};
 
 //Get Requested Pairs & recent PriceQueue from "Pairs" Table
 const getPairs = async () => {
@@ -42,107 +40,91 @@ const getPairs = async () => {
 };
 
 const getTrades = async () => {
-  let { data: Trades, error } = await supabase.from('Trades').select('*')
-  return Trades; 
+  let { data: Trades, error } = await supabase.from('Trades').select('*');
+  return Trades;
 };
 
-// Mark as complete 
+// Mark as complete
 const markTradeAsComplete = async (id: number) => {
-
   const { data, error } = await supabase
-    .from("Trades")
+    .from('Trades')
     .update({
-      complete: true, 
-      ready: false
+      complete: true,
+      ready: false,
     })
-    .match({id: id}); 
+    .match({ id: id });
 
-  if (error){
+  if (error) {
     console.error('Error updating row:', error);
   }
-}
-
+};
 
 const updateRemainingBatches = async (id: number, batches_left: number) => {
-
   const { data, error } = await supabase
-    .from("Trades")
+    .from('Trades')
     .update({
-      remainingBatches: batches_left, 
+      remainingBatches: batches_left,
     })
-    .match({id: id}); 
+    .match({ id: id });
 
-  if (error){
+  if (error) {
     console.error('Error updating row:', error);
   }
-}
+};
 
 const updateTimings = async (id: number, timings: Record<string, any>) => {
-
   const { data, error } = await supabase
-    .from("Trades")
+    .from('Trades')
     .update({
-      batch_timings: timings, 
+      batch_timings: timings,
     })
-    .match({id: id}); 
+    .match({ id: id });
 
-  if (error){
+  if (error) {
     console.error('Error updating row:', error);
   }
-
-}
-
+};
 
 const updateTradeBatchTimings = async () => {
-
-  let trades = await getTrades(); 
+  let trades = await getTrades();
 
   for (let trade of trades) {
+    let events = await getContractLogEvents(trade.signature);
+    const numberOfEvents = events.length;
 
-    let events = await getContractLogEvents(trade.signature); 
-    const numberOfEvents = events.length; 
-
-    // Update trade as complete if batches are over 
-    if (trade.batches == numberOfEvents){
-      markTradeAsComplete(trade.id); 
+    // Update trade as complete if batches are over
+    if (trade.batches == numberOfEvents) {
+      markTradeAsComplete(trade.id);
     } else {
-
-      let remainingBatches = trade.batches - numberOfEvents; 
-      updateRemainingBatches(trade.id, remainingBatches); 
+      let remainingBatches = trade.batches - numberOfEvents;
+      updateRemainingBatches(trade.id, remainingBatches);
 
       // Check if batch_timings data exists (i.e. not null)
-      if (trade.batch_timings){
-        
+      if (trade.batch_timings) {
         const numberOfExecutedBatches = Object.keys(trade.batchTimings).length;
 
-        // Update most recent executed batch 
-        if (numberOfExecutedBatches + 1 == numberOfEvents){ 
+        // Update most recent executed batch
+        if (numberOfExecutedBatches + 1 == numberOfEvents) {
           const newBatchNumber = Object.keys(trade.batchTimings).length + 1;
-          const newBatchTimestamp = new Date().getTime(); // Get current unix timestamp 
+          const newBatchTimestamp = new Date().getTime(); // Get current unix timestamp
           trade.batchTimings[newBatchNumber.toString()] = newBatchTimestamp;
 
-          updateTimings(trade.id, trade.batch_timings)
+          updateTimings(trade.id, trade.batch_timings);
         }
-        
       } else {
-
-        // Data doesn't exist yet - first batch execution 
+        // Data doesn't exist yet - first batch execution
         const current_time = new Date();
-        const timestamp = current_time.getTime(); // UNIX timestamp 
+        const timestamp = current_time.getTime(); // UNIX timestamp
 
         const batch_timings = {
-          "1" : timestamp
-        }; 
+          '1': timestamp,
+        };
 
-        updateTimings(trade.id, batch_timings); 
+        updateTimings(trade.id, batch_timings);
       }
-
     }
-
-
   }
-
-}
+};
 
 const updateTrades = async () => {
   let pairs = await getPairs();
@@ -154,14 +136,18 @@ const updateTrades = async () => {
       .eq('ready', 'true')
       .eq('pair', pair.path);
 
-    let allPrices = pair['daily_prices'].priceFeed;
-    let currentPrice = allPrices[allPrices.length - 1];
+    let currentOutput = pair['current_price'];
 
     for (let trade of Trades) {
-      let allMeanPrices =
-        pair[`${trade.order.avgPrice}min_avg`]['close_prices'];
+      let allMeanPrices;
+      try {
+        allMeanPrices = pair[`${trade.order.priceAvg}min_avg`]['close_prices'];
+      } catch (e) {
+        console.log(`error with ${pair}`);
+        break;
+      }
       if (!allMeanPrices || allMeanPrices.length == 0) {
-        console.log('hit an error');
+        console.log(`error with ${pair}`);
         break;
       }
       // Convert each string to a float and calculate the sum
@@ -169,44 +155,28 @@ const updateTrades = async () => {
       // Calculate the average
       let movingAveragePrice = sum / allMeanPrices.length;
 
-      const current_time = new Date().getTime(); // UNIX timestamp 
-      const mostRecentBatch = Object.keys(trade.batch_timings).length; 
-      const lastBatchTime = trade.batch_timings[mostRecentBatch.toString()]; 
-      const timeBetweenBatches = trade.time_bw_batches; 
+      const current_time = new Date().getTime(); // UNIX timestamp
+      const mostRecentBatch = Object.keys(trade.batch_timings).length;
+      const lastBatchTime = trade.batch_timings[mostRecentBatch.toString()];
+      const timeBetweenBatches = trade.time_bw_batches;
 
-      // Only mark trade as ready if time between batches is satisfied 
-      if (timeBetweenBatches >= (current_time - lastBatchTime)){
-
-        if (trade.order.tradeOption == 'buy') {
-          let buyUnder =
-            ((100 - trade.order.tradeDelta) / 100) * movingAveragePrice;
-          if (currentPrice <= buyUnder) {
-            const { data, error } = await supabase
-              .from('Trades')
-              .update({ ready: 'true' })
-              .eq('id', trade.id)
-              .select();
-            console.log(data || error);
-          }
+      // Only mark trade as ready if time between batches is satisfied
+      if (timeBetweenBatches >= current_time - lastBatchTime) {
+        let buyOutputOver =
+          ((100 + Number(trade.order.percentChange)) / 100) *
+          movingAveragePrice;
+        if (currentOutput >= buyOutputOver) {
+          const { data, error } = await supabase
+            .from('Trades')
+            .update({ ready: 'true' })
+            .eq('id', trade.id)
+            .select();
+          console.log(data || error);
         }
-        if (trade.order.tradeOption == 'sell') {
-          let sellOver =
-            ((100 + trade.order.tradeDelta) / 100) * movingAveragePrice;
-          if (currentPrice >= sellOver) {
-            const { data, error } = await supabase
-              .from('Trades')
-              .update({ ready: 'true' })
-              .eq('id', trade.id)
-              .select();
-            console.log(data || error);
-          }
-        }
-
       }
-
     }
   }
 };
 
-updateTradeBatchTimings(); 
+updateTradeBatchTimings();
 updateTrades();
