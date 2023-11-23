9e736332dd80af4ca6ee03a5342af0d1daae8a6d
“Karthik144” <karthik.k.ramu@gmail.com>
2023-11-23 14:22:01 -0500

    Added call data logic into evaluate microservice

--- src/backend/evaluate_microservice/index.ts
+++ src/backend/evaluate_microservice/index.ts
@@ -124,58 +124,6 @@ const updateTradeBatchTimings = async () => {
   }
 };
 
-const updateTrades = async () => {
-  let pairs = await getPairs();
-
-  for (let pair of pairs) {
-    let { data: Trades, error } = await supabase
-      .from('Trades')
-      .select('*')
-      .eq('ready', 'false')
-      .eq('pair', pair.path);
-
-    let currentOutput = pair['current_price'];
-
-    for (let trade of Trades) {
-      let allMeanPrices;
-      try {
-        allMeanPrices = pair[`${trade.order.priceAvg}min_avg`]['close_prices'];
-      } catch (e) {
-        console.log(`error with ${pair}`);
-        break;
-      }
-      if (!allMeanPrices || allMeanPrices.length == 0) {
-        console.log(`error with ${pair}`);
-        break;
-      }
-      // Convert each string to a float and calculate the sum
-      let sum = allMeanPrices.reduce((acc: any, val: number) => acc + val, 0);
-      // Calculate the average
-      let movingAveragePrice = sum / allMeanPrices.length;
-
-      const current_time = new Date().getTime(); // UNIX timestamp
-      const mostRecentBatch = Object.keys(trade.batch_timings).length;
-      const lastBatchTime = trade.batch_timings[mostRecentBatch.toString()];
-      const timeBetweenBatches = trade.time_bw_batches;
-
-      // Only mark trade as ready if time between batches is satisfied
-      if (timeBetweenBatches >= current_time - lastBatchTime) {
-        let buyOutputOver =
-          ((100 + Number(trade.order.percentChange)) / 100) *
-          movingAveragePrice;
-        if (currentOutput >= buyOutputOver) {
-          const { data, error } = await supabase
-            .from('Trades')
-            .update({ ready: 'true' })
-            .eq('id', trade.id)
-            .select();
-          console.log(data || error);
-        }
-      }
-    }
-  }
-};
-
 // COPY PASTED FROM WEB3FUNCTIONS - NEED TO EDIT
 async function fetchQuote(
   pair: {
@@ -216,6 +164,85 @@ function getDeconstructedCalldata(calldata: { data: any }): object {
 
   return ZeroExStruct;
 }
+
+async function getSwapCallData(inputTokenAddress: string, outputTokenAddress: string, amount: string){
+
+  let apiUrl = 'OX_API_URL';
+  let apiKey = '0X_API_KEY';
+
+  const tokenPair = {
+    input: inputTokenAddress, 
+    output: outputTokenAddress,
+    amount: amount,
+  };
+
+  try {
+    let zeroX_quote = await fetchQuote(tokenPair, apiKey, apiUrl!);
+    const swapCallData = getDeconstructedCalldata(zeroX_quote);
+    return swapCallData; 
+  } catch (error){
+    console.error('Error in getSwapCallData:', error);
+  }
+
+}
+
+const updateTrades = async () => {
+  let pairs = await getPairs();
+
+  for (let pair of pairs) {
+    let { data: Trades, error } = await supabase
+      .from('Trades')
+      .select('*')
+      .eq('ready', 'false')
+      .eq('pair', pair.path);
+
+    let currentOutput = pair['current_price'];
+
+    for (let trade of Trades) {
+      let allMeanPrices;
+      try {
+        allMeanPrices = pair[`${trade.order.priceAvg}min_avg`]['close_prices'];
+      } catch (e) {
+        console.log(`error with ${pair}`);
+        break;
+      }
+      if (!allMeanPrices || allMeanPrices.length == 0) {
+        console.log(`error with ${pair}`);
+        break;
+      }
+      // Convert each string to a float and calculate the sum
+      let sum = allMeanPrices.reduce((acc: any, val: number) => acc + val, 0);
+      // Calculate the average
+      let movingAveragePrice = sum / allMeanPrices.length;
+
+      const current_time = new Date().getTime(); // UNIX timestamp
+      const mostRecentBatch = Object.keys(trade.batch_timings).length;
+      const lastBatchTime = trade.batch_timings[mostRecentBatch.toString()];
+      const timeBetweenBatches = trade.time_bw_batches;
+
+      // Get swap call data 
+      const callData = getSwapCallData(trade.order.inputTokenAddy, trade.order.outputTokenAddy, trade.order.amount); 
+
+      // Only mark trade as ready if time between batches is satisfied
+      if (timeBetweenBatches >= current_time - lastBatchTime) {
+        let buyOutputOver =
+          ((100 + Number(trade.order.percentChange)) / 100) *
+          movingAveragePrice;
+        if (currentOutput >= buyOutputOver) {
+          const { data, error } = await supabase
+            .from('Trades')
+            .update({ 
+              ready: true, 
+              zero_x_call_data: callData
+            })
+            .eq('id', trade.id)
+            .select();
+          console.log(data || error);
+        }
+      }
+    }
+  }
+};
 
 updateTradeBatchTimings();
 updateTrades();
