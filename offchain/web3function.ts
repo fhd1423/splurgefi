c4aeaf5c339447c6446fd1bf7e748974ebef6dd0
Rohith Venkatesan <rohithvenkatesan@Rohiths-MacBook-Pro.local>
2023-12-06 23:46:26 -0500

    prevent double execution before tradeupdate in web3function

--- offchain/web3function.ts
+++ offchain/web3function.ts
@@ -4,24 +4,32 @@ import {
 } from '@gelatonetwork/web3-functions-sdk';
 import { createClient } from '@supabase/supabase-js';
 
-async function fetchReadyTrades(supabaseUrl: string, supabaseKey: string) {
+async function fetchReadyTrades(
+  supabaseUrl: string,
+  supabaseKey: string,
+  storage: any,
+) {
   const supabase = createClient(supabaseUrl!, supabaseKey!);
 
   const { data: Trades, error } = await supabase
     .from('Trades')
-    .select('zero_x_call_data')
+    .select('*')
     .eq('ready', true);
 
   if (error) {
     console.error('Error fetching data:', error);
     return;
   }
-  if (Trades[0]) return Trades[0].zero_x_call_data;
+  if (Trades[0] && Trades[0].id != (await storage.get('lastExecuted'))) {
+    await storage.set('lastExecuted', Trades[0].id);
+    return Trades[0].zero_x_call_data;
+  }
   return null;
 }
 
 Web3Function.onRun(async (context: Web3FunctionContext) => {
-  const { userArgs, gelatoArgs, secrets, multiChainProvider } = context;
+  const { userArgs, gelatoArgs, storage, secrets, multiChainProvider } =
+    context;
   const provider = multiChainProvider.default();
   const splurgeAddy = await secrets.get('SPLURGE_ADDY');
   const supabaseUrl = await secrets.get('SUPABASE_URL');
@@ -29,7 +37,7 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
   if (!supabaseUrl) throw new Error(`no supabase url provided`);
   if (!supabaseKey) throw new Error(`no supbase key provided`);
   if (!splurgeAddy) throw new Error(`no supabase url provided`);
-  const readyTrades = await fetchReadyTrades(supabaseUrl, supabaseKey);
+  const readyTrades = await fetchReadyTrades(supabaseUrl, supabaseKey, storage);
 
   if (readyTrades) {
     return {
