309b6e0ad490912ebc78adfb00117d39e50ab5d4
Rohith Venkatesan <rohithvenkatesan@Rohiths-MacBook-Pro.local>
2024-01-02 18:14:29 -0500

    retry mapping

--- simulate.sh
+++ simulate.sh
@@ -7,7 +7,7 @@ DeployerTags="--private-key $PRIVATE_KEY --rpc-url https://polygon-mumbai.g.alch
 TraderTags="--private-key $PRIVATE_KEY_TRADER --rpc-url https://polygon-mumbai.g.alchemy.com/v2/9Lgo55iX-WyUfLGeAL5fy5YIFf1hu--B"
 
 #deploy splurge
-splurgeDeployment=$(forge create src/contracts/src/splurge.sol:Splurge --constructor-args 0xf471d32cb40837bf24529fcf17418fc1a4807626 0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889 0x8839278a75dc8249bc0c713a710aaebd0fee6750 $DeployerTags --verify --etherscan-api-key 7CZYAQD27GJ9MZF6Y8NAQYWRKMV7E8N5SS) # arbi: YS7YS943N4358NK6AQJ411JAWCBQZXD4QA
+splurgeDeployment=$(forge create src/contracts/splurge.sol:Splurge $DeployerTags --verify --etherscan-api-key 7CZYAQD27GJ9MZF6Y8NAQYWRKMV7E8N5SS) # arbi: YS7YS943N4358NK6AQJ411JAWCBQZXD4QA
 splurgeContract=$(echo "$splurgeDeployment" | awk -F": " '/Deployed to:/ {print $2}')
 echo splurge: $splurgeContract
 
--- src/contracts/tests/splurge.t.sol
+++ src/contracts/tests/splurge.t.sol
@@ -1,7 +1,7 @@
-// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
 /*
 
-pragma solidity ^0.8.13;
 
 import { Test } from "forge-std/Test.sol";
 import { Splurge } from "../src/splurge.sol";
@@ -29,158 +29,158 @@ contract SplurgeTest is Test {
         // realPrivateKey = 0xd9ed762bc42f7913160dffd48b19c3d55bb3c76e2fadabda232e386fb43fd0f6;
     }
 */
-    //solhint-disable-next-line
-    // function testFuzz_TakeFees(uint256 amount) public {
-    //     uint256 postFeeAmount = splurgeContract.takeFees(amount);
-    //     uint256 gasPaid = tradeGasLimit * tx.gasprice;
-    //     uint256 afterGas = amount - gasPaid;
-    //     uint256 expectedFee = (afterGas * 9985) / 10000;
-    //     assertTrue(postFeeAmount == expectedFee);
-    // }
+//solhint-disable-next-line
+// function testFuzz_TakeFees(uint256 amount) public {
+//     uint256 postFeeAmount = splurgeContract.takeFees(amount);
+//     uint256 gasPaid = tradeGasLimit * tx.gasprice;
+//     uint256 afterGas = amount - gasPaid;
+//     uint256 expectedFee = (afterGas * 9985) / 10000;
+//     assertTrue(postFeeAmount == expectedFee);
+// }
 
-    // function test_getSigner() public view {
-    //     (uint8 v, bytes32 r, bytes32 s) = vm.sign(
-    //         ownerPrivateKey,
-    //         keccak256(abi.encodePacked("hi"))
-    //     );
+// function test_getSigner() public view {
+//     (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+//         ownerPrivateKey,
+//         keccak256(abi.encodePacked("hi"))
+//     );
 
-    //     bytes memory signature = joinSignature(v, r, s);
+//     bytes memory signature = joinSignature(v, r, s);
 
-    //     //require(splurgeContract.verifyTrade("hi", signature) == owner);
-    // }
+//     //require(splurgeContract.verifyTrade("hi", signature) == owner);
+// }
 
-    // function testVerifyTradeDetails() public view {
-    //     address inputTokenAddy = 0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889; // wmatic
-    //     address outputTokenAddy = 0xa0a6c157871A9F38253234BBfD2B8D79F9e9FCDC; // token1
-    //     address recipient = 0x8839278A75dc8249BC0C713A710aaEBD0FEE6750;
-    //     string memory orderType = "buy";
-    //     uint amount = 10000;
-    //     uint tranches = 6;
-    //     uint percentChange = 15;
-    //     uint priceAvg = 4;
-    //     uint deadline = 1730016559; // date in 2024
-    //     uint timeBwTrade = 100;
-    //     uint slippage = 1;
-    //     uint salt = 1;
+// function testVerifyTradeDetails() public view {
+//     address inputTokenAddy = 0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889; // wmatic
+//     address outputTokenAddy = 0xa0a6c157871A9F38253234BBfD2B8D79F9e9FCDC; // token1
+//     address recipient = 0x8839278A75dc8249BC0C713A710aaEBD0FEE6750;
+//     string memory orderType = "buy";
+//     uint amount = 10000;
+//     uint tranches = 6;
+//     uint percentChange = 15;
+//     uint priceAvg = 4;
+//     uint deadline = 1730016559; // date in 2024
+//     uint timeBwTrade = 100;
+//     uint slippage = 1;
+//     uint salt = 1;
 
-    //     SplurgeOrderStruct memory order = SplurgeOrderStruct(
-    //         inputTokenAddy,
-    //         outputTokenAddy,
-    //         recipient,
-    //         // orderType,
-    //         amount,
-    //         tranches,
-    //         percentChange,
-    //         priceAvg,
-    //         deadline,
-    //         timeBwTrade,
-    //         slippage,
-    //         salt
-    //     );
+//     SplurgeOrderStruct memory order = SplurgeOrderStruct(
+//         inputTokenAddy,
+//         outputTokenAddy,
+//         recipient,
+//         // orderType,
+//         amount,
+//         tranches,
+//         percentChange,
+//         priceAvg,
+//         deadline,
+//         timeBwTrade,
+//         slippage,
+//         salt
+//     );
 
-    //     bytes memory concatenatedOrderBytesBeforeHash = abi.encode(
-    //         order.inputTokenAddy,
-    //         order.outputTokenAddy,
-    //         order.recipient,
-    //         // order.orderType,
-    //         order.amount,
-    //         order.tranches,
-    //         order.percentChange,
-    //         order.priceAvg,
-    //         order.deadline,
-    //         order.timeBwTrade,
-    //         order.slippage,
-    //         order.salt
-    //     );
+//     bytes memory concatenatedOrderBytesBeforeHash = abi.encode(
+//         order.inputTokenAddy,
+//         order.outputTokenAddy,
+//         order.recipient,
+//         // order.orderType,
+//         order.amount,
+//         order.tranches,
+//         order.percentChange,
+//         order.priceAvg,
+//         order.deadline,
+//         order.timeBwTrade,
+//         order.slippage,
+//         order.salt
+//     );
 
-    //     (uint8 v, bytes32 r, bytes32 s) = vm.sign(
-    //         realPrivateKey,
-    //         keccak256(abi.encodePacked(concatenatedOrderBytesBeforeHash))
-    //     );
+//     (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+//         realPrivateKey,
+//         keccak256(abi.encodePacked(concatenatedOrderBytesBeforeHash))
+//     );
 
-    //     bytes memory signature = joinSignature(v, r, s);
-    //     console.logBytes(signature);
+//     bytes memory signature = joinSignature(v, r, s);
+//     console.logBytes(signature);
 
-    //     require(
-    //         splurgeContract.verifyTrade(
-    //             concatenatedOrderBytesBeforeHash,
-    //             signature
-    //         ) == recipient
-    //     );
-    // }
+//     require(
+//         splurgeContract.verifyTrade(
+//             concatenatedOrderBytesBeforeHash,
+//             signature
+//         ) == recipient
+//     );
+// }
 
-    // function testPrepareVerifyTrade() public {
-    //     address inputTokenAddy = vm.addr(0x11);
-    //     address outputTokenAddy = vm.addr(0x22);
-    //     address recipient = owner;
-    //     uint amount = 100000;
-    //     uint deadline = 1730016559; // date in 2024
-    //     uint8 salt = 1;
-    //     uint8 tranches = 6;
-    //     SplurgeOrderStruct memory order = SplurgeOrderStruct(
-    //         inputTokenAddy,
-    //         outputTokenAddy,
-    //         recipient,
-    //         amount,
-    //         tranches,
-    //         deadline,
-    //         salt
-    //     );
+// function testPrepareVerifyTrade() public {
+//     address inputTokenAddy = vm.addr(0x11);
+//     address outputTokenAddy = vm.addr(0x22);
+//     address recipient = owner;
+//     uint amount = 100000;
+//     uint deadline = 1730016559; // date in 2024
+//     uint8 salt = 1;
+//     uint8 tranches = 6;
+//     SplurgeOrderStruct memory order = SplurgeOrderStruct(
+//         inputTokenAddy,
+//         outputTokenAddy,
+//         recipient,
+//         amount,
+//         tranches,
+//         deadline,
+//         salt
+//     );
 
-    //     bytes memory concatenatedOrderBytesBeforeHash = abi.encode(
-    //         order.inputTokenAddy,
-    //         order.outputTokenAddy,
-    //         order.recipient,
-    //         order.amount,
-    //         order.tranches,
-    //         order.deadline,
-    //         order.salt
-    //     );
+//     bytes memory concatenatedOrderBytesBeforeHash = abi.encode(
+//         order.inputTokenAddy,
+//         order.outputTokenAddy,
+//         order.recipient,
+//         order.amount,
+//         order.tranches,
+//         order.deadline,
+//         order.salt
+//     );
 
-    //     (uint8 v, bytes32 r, bytes32 s) = vm.sign(
-    //         ownerPrivateKey,
-    //         keccak256(abi.encodePacked(concatenatedOrderBytesBeforeHash))
-    //     );
+//     (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+//         ownerPrivateKey,
+//         keccak256(abi.encodePacked(concatenatedOrderBytesBeforeHash))
+//     );
 
-    //     bytes memory signature = joinSignature(v, r, s);
+//     bytes memory signature = joinSignature(v, r, s);
 
-    //     vm.expectRevert();
+//     vm.expectRevert();
 
-    //     // Create a Transformation instance
-    //     Transformation memory transformation1 = Transformation({
-    //         deploymentNonce: 0, // replace 0 with your desired nonce
-    //         data: new bytes(0) // replace with your desired data
-    //     });
+//     // Create a Transformation instance
+//     Transformation memory transformation1 = Transformation({
+//         deploymentNonce: 0, // replace 0 with your desired nonce
+//         data: new bytes(0) // replace with your desired data
+//     });
 
-    //     // Create an array of Transformations
-    //     Transformation[] memory transformationsArray = new Transformation[](1);
-    //     transformationsArray[0] = transformation1;
+//     // Create an array of Transformations
+//     Transformation[] memory transformationsArray = new Transformation[](1);
+//     transformationsArray[0] = transformation1;
 
-    //     // Use the array in ZeroExSwapStruct
-    //     ZeroExSwapStruct memory zeroExSwap = ZeroExSwapStruct({
-    //         inputToken: vm.addr(0),
-    //         outputToken: vm.addr(0),
-    //         inputTokenAmount: 0,
-    //         minOutputTokenAmount: 0,
-    //         transformations: transformationsArray
-    //     });
+//     // Use the array in ZeroExSwapStruct
+//     ZeroExSwapStruct memory zeroExSwap = ZeroExSwapStruct({
+//         inputToken: vm.addr(0),
+//         outputToken: vm.addr(0),
+//         inputTokenAmount: 0,
+//         minOutputTokenAmount: 0,
+//         transformations: transformationsArray
+//     });
 
-    //     // this will revert since its not a real contract anyways, have to mock it somehow
-    //     splurgeContract.verifyExecuteTrade(order, signature, zeroExSwap);
-    // }
+//     // this will revert since its not a real contract anyways, have to mock it somehow
+//     splurgeContract.verifyExecuteTrade(order, signature, zeroExSwap);
+// }
 
-    //Utility
-    // function joinSignature(
-    //     uint8 v,
-    //     bytes32 r,
-    //     bytes32 s
-    // ) public pure returns (bytes memory) {
-    //     bytes memory sig = new bytes(65);
-    //     assembly {
-    //         mstore(add(sig, 32), r)
-    //         mstore(add(sig, 64), s)
-    //     }
-    //     sig[64] = bytes1(v);
-    //     return sig;
-    // }
+//Utility
+// function joinSignature(
+//     uint8 v,
+//     bytes32 r,
+//     bytes32 s
+// ) public pure returns (bytes memory) {
+//     bytes memory sig = new bytes(65);
+//     assembly {
+//         mstore(add(sig, 32), r)
+//         mstore(add(sig, 64), s)
+//     }
+//     sig[64] = bytes1(v);
+//     return sig;
+// }
 //}
