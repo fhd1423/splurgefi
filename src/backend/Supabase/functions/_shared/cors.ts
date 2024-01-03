1904ae5ff01cf741007d5e88bb0e627166432a3a
Rohith Venkatesan <rohithvenkatesan@Rohiths-MacBook-Pro.local>
2024-01-03 10:26:12 -0500

    new edge functions

--- src/backend/Supabase/functions/_shared/cors.ts
+++ src/backend/Supabase/functions/_shared/cors.ts
@@ -1,5 +1,5 @@
 export const corsHeaders = {
-  'Access-Control-Allow-Origin': 'https://www.splurgefi.xyz/',
+  'Access-Control-Allow-Origin': 'https://splurgefi.xyz',
   'Access-Control-Allow-Headers':
     'authorization, x-client-info, apikey, content-type',
   'Access-Control-Allow-Methods': 'OPTIONS, POST',
